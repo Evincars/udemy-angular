a5010bca47930a6a469594bad423fc33c5fc8812
lasakada <adam.lasak@tieto.com>
2020-05-31 13:44:48 +0200

    reactive forms completed

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
+// import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
 import { CardComponent } from './card/card.component';
 
-export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
+// export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
-  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
+  declarations: [
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule /*NgxMaskModule.forRoot(options)*/,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- creditcard/src/app/card/card.component.ts
+++ creditcard/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  @Input() cardNumber: string;
+  @Input() name: string;
+  @Input() expiration: string;
+  
   constructor() { }
 
   ngOnInit() {
