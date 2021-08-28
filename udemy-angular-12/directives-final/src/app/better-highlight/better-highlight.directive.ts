218cd3455a2e21d4fc6e44087b584cadaf437bfd
pr033r <lasak.ad@gmail.com>
2021-08-28 10:33:37 +0200

    comment from Stack Overflow

--- udemy-angular-12/directives-final/src/app/better-highlight/better-highlight.directive.ts
+++ udemy-angular-12/directives-final/src/app/better-highlight/better-highlight.directive.ts
@@ -12,6 +12,17 @@ import {
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  /*
+  A quick tip that helps me remember what they do -
+  HostBinding('value') myValue; is exactly the same as [value]="myValue"
+
+  And
+
+  HostListener('click') myClick(){ } is exactly the same as (click)="myClick()"
+
+  HostBinding and HostListener are written in directives and the other ones 
+  (...) and [..] are written inside templates (of components).
+  */
   @Input() defaultColor: string = 'transparent';
   @Input('appBetterHighlight') highlightColor: string = 'blue';
   @HostBinding('style.backgroundColor') backgroundColor: string;
