df89847b6cb1bfa39cd93a510f05eb6485c4cd9b
pr033r <lasak.ad@gmail.com>
2021-09-10 14:28:13 +0200

    http tutorial completed

---
+++ udemy-angular-12/http-09-finished/src/app/auth-interceptor.service.ts
@@ -0,0 +1,14 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
+  }
+}
