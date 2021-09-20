569952032ff656689d8ebcc9eadf2607d50249bc
pr033r <lasak.ad@gmail.com>
2021-09-20 17:06:42 +0200

    Web Workers & Animations

---
+++ udemy-angular-12/animations-finished/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
