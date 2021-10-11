8ac8e62fbc692dd4f25ceacac55d2816d2ee9e5e
Adam Lasak <lasak.ad@gmail.com>
2021-10-11 12:37:51 +0200

    dashboard added

--- udemy-angular-12/angular-config/src/app/app.module.ts
+++ udemy-angular-12/angular-config/src/app/app.module.ts
@@ -10,11 +10,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,10 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
